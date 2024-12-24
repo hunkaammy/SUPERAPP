2a610a8e81f5d76cabe35900ee881832bc7fb8c7
hunkaammy <77635140+hunkaammy@users.noreply.github.com>
2024-12-24 13:36:38 +0530

    y

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('service-worker.js')
             .then(registration => {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                console.log('ServiceWorker registered with scope: ', registration.scope);
             })
             .catch(error => {
                 console.log('ServiceWorker registration failed: ', error);
